d749826143c59a07b8ee19208c9407046dc153f7
Juliana Morais <julianamm88@gmail.com>
2019-12-20 16:15:52 -0800

    create components UserInput and UserOutput

---
+++ src/Components/UserInput.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class UserInput extends Component {
+    render() {
+        return (
+        <div className='App'>
+            <input type='text'/>
+        </div>
+        );
+    }
+     
+}
+
+export default UserInput;
